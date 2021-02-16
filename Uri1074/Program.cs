0f7fc9222c7761ed76a8437bf5c84ed77cf15d43
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 14:40:43 -0300

    Add files via upload

---
+++ Uri1074/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Uri1074
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, num;
+
+            n = int.Parse(Console.ReadLine());
+
+            for (int c = 1; c <= 4; c++)
+            {
+                num = int.Parse(Console.ReadLine());
+
+                if (num == 0)
+                {
+                    Console.WriteLine("Null");
+                }
+                if (num % 2 == 0 && num != 0)
+                {
+                    Console.Write("Even ");
+                    if (num > 0)
+                    {
+                        Console.WriteLine("Positive");
+                    }
+                    else if (num < 0)
+                    {
+                        Console.WriteLine("Negative");
+                    }
+                }
+                else if (num % 2 != 0 && num != 0)
+                {
+                    Console.Write("Odd ");
+                    if (num > 0)
+                    {
+                        Console.WriteLine("Positive");
+                    }
+                    else if (num < 0)
+                    {
+                        Console.WriteLine("Negative");
+                    }
+                }
+            }
+        }
+    }
+}
